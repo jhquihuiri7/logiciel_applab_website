e4aecbaee7a01b04e90480289ff3365c51f122f3
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-01-21 15:03:08 -0600

    cards added

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,15 +2,14 @@ import React, { FunctionComponent } from 'react';
 import Header from './header';
 import Footer from './footer';
 import Nosotros from './nosotros';
+import Clients from './clients'
 
 const DoraAIAModernFuturistic:FunctionComponent = () => {
   	return (
 		<div className="w-full relative bg-white text-left text-base text-[#86858c] font-dm-sans">
 			<Header></Header>
             <Nosotros></Nosotros>
-			<div className='bg-black w-full h-[500px]'>
-
-			</div>
+			<Clients></Clients>
 			<Footer></Footer>
     	</div>
 		);
