7aeb0f27f0742ac67bd983434bb88b41ee38508e
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-01-22 10:49:25 -0600

    font changed and favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Lora } from "next/font/google";
 import "./globals.css";
+
+const loraFont = Lora({
+  variable: "--font-lora",
+  subsets: ["latin"],
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +28,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${loraFont.variable} ${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
